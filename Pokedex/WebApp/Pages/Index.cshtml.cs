40f6fac9d99c32b8169730f511287ad6ab714871
Constance <constance.carle@epita.fr>
2023-05-20 20:49:37 +0200

    checkbox but the pages reloads...

--- Pokedex/WebApp/Pages/Index.cshtml.cs
+++ Pokedex/WebApp/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.IdentityModel.Tokens;
 
 namespace WebApp.Pages
 {
@@ -7,6 +8,7 @@ namespace WebApp.Pages
     {
         private readonly HttpClient _httpClient;
         public Task<IEnumerable<Pokemon>> RegisteredPokemons { get; set; }
+        public Task<IEnumerable<User>> RegisteredUsers { get; set; }
 
         [BindProperty(SupportsGet = true)]
         public string SearchQuery { get; set; }
@@ -14,23 +16,75 @@ namespace WebApp.Pages
         [BindProperty(SupportsGet = true)]
         public string SelectedType { get; set; }
 
+        [BindProperty(SupportsGet = true)]
         public bool ShowFavorites { get; set; }
-
+        
+        private async Task<IEnumerable<User>> FetchUsersList()
+        {
+            var usersResponse = await _httpClient.GetAsync("api/User");
+            if (!usersResponse.IsSuccessStatusCode)
+                return Enumerable.Empty<User>();
+            RegisteredUsers = usersResponse.Content.ReadFromJsonAsync<IEnumerable<User>>();
+            return await RegisteredUsers;
+        }
+        
         public IndexModel(IConfiguration defaultConfig)
         {
             _httpClient = new HttpClient { BaseAddress = new Uri(defaultConfig.GetValue<String>("apiHost")) };
         }
 
-        private async Task FetchPokemonList()
+        public Task<IEnumerable<Pokemon>> GetAll(string includeTables = "")
+        {
+            var pokemons = new List<Pokemon>
+            {
+                new Pokemon { Id = 1, Name = "Pikachu", Type1 = "Electrique", ImagePath = "/Screenshot_2.jpg"},
+                new Pokemon { Id = 2, Name = "Salamèche", Type1 = "Feu", Type2 = "Normal", ImagePath = "/Salameche.png"},
+                new Pokemon { Id = 3, Name = "Dummy1", Type1 = "Eau", ImagePath = "/Screenshot_2.jpg" },
+                new Pokemon { Id = 4, Name = "Dummy2", Type1 = "Acier", ImagePath = "/Screenshot_2.jpg" },
+                new Pokemon { Id = 5, Name = "Dummy3", Type1 = "Ténèbres", ImagePath = "/Screenshot_2.jpg" },
+                new Pokemon { Id = 6, Name = "Dummy4", Type1 = "Spectre", ImagePath = "/Screenshot_2.jpg" },
+                new Pokemon { Id = 7, Name = "Dummy5", Type1 = "Dragon", ImagePath = "/Screenshot_2.jpg" },
+                new Pokemon { Id = 8, Name = "Dummy6", Type1 = "Insecte", ImagePath = "/Screenshot_2.jpg" },
+                new Pokemon { Id = 9, Name = "Dummy7", Type1 = "Glace", ImagePath = "/Screenshot_2.jpg" },
+                new Pokemon { Id = 10, Name = "Dummy8", Type1 = "Roche", ImagePath = "/Screenshot_2.jpg" },
+                new Pokemon { Id = 11, Name = "Dummy9", Type1 = "Sol", ImagePath = "/Screenshot_2.jpg" },
+                new Pokemon { Id = 12, Name = "Dummy10", Type1 = "Poison", ImagePath = "/Screenshot_2.jpg" },
+                new Pokemon { Id = 13, Name = "Dummy11", Type1 = "Vol", ImagePath = "/Screenshot_2.jpg" },
+                new Pokemon { Id = 14, Name = "Dummy12", Type1 = "Psy", ImagePath = "/Screenshot_2.jpg" },
+                new Pokemon { Id = 15, Name = "Dummy13", Type1 = "Electrique", ImagePath = "/Screenshot_2.jpg" },
+                new Pokemon { Id = 16, Name = "Dummy14", Type1 = "Combat", ImagePath = "/Screenshot_2.jpg" },
+                new Pokemon { Id = 17, Name = "Dummy15", Type1 = "Normal", ImagePath = "/Screenshot_2.jpg" },
+                new Pokemon { Id = 18, Name = "Dummy16", Type1 = "Plante", ImagePath = "/Screenshot_2.jpg" }
+            };
+
+            return Task.FromResult<IEnumerable<Pokemon>>(pokemons);
+        }
+        
+        private async Task<IEnumerable<Pokemon>> FetchPokemonList()
         {
             var pokemonsResponse = await _httpClient.GetAsync("api/Pokemon");
-            if (!pokemonsResponse.IsSuccessStatusCode) return;
+            if (!pokemonsResponse.IsSuccessStatusCode)
+                return Enumerable.Empty<Pokemon>();
             RegisteredPokemons = pokemonsResponse.Content.ReadFromJsonAsync<IEnumerable<Pokemon>>();
+            return await RegisteredPokemons;
         }
 
-        private async Task FilterPokemons()
+        private async Task FilterPokemons(bool showFavorites)
         {
             var pokemons = await RegisteredPokemons;
+            
+            if (showFavorites)
+            {
+                var userId = HttpContext.Request.Cookies["user"];
+                var users = await FetchUsersList();
+                var user = users.FirstOrDefault(u => u.Id.ToString() == userId);
+                
+                if (user != null)
+                {
+                    RegisteredPokemons = Task.FromResult(pokemons.Where(pokemon => user.Pokemons.Contains(pokemon.Id)));
+                }
+            }
+            
             if (!string.IsNullOrEmpty(SelectedType) && !SelectedType.Equals("Tous les types"))
             {
                 RegisteredPokemons = Task.FromResult(pokemons.Where(pokemon =>
@@ -54,14 +108,21 @@ namespace WebApp.Pages
         {
             SearchQuery = searchQuery;
             await FetchPokemonList();
-            await FilterPokemons();
+            await FilterPokemons(ShowFavorites);
         }
         
         public async Task OnPostFilter([FromForm] string selectedType)
         {
             SelectedType = selectedType;
             await FetchPokemonList();
-            await FilterPokemons();
+            await FilterPokemons(ShowFavorites);
+        }
+        
+        public async Task OnPostFavorites([FromForm] string showFavorites)
+        {
+            ShowFavorites = !showFavorites.IsNullOrEmpty();
+            await FetchPokemonList();
+            await FilterPokemons(ShowFavorites);
         }
     }
 }
