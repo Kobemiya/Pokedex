9eb389c75ec5ed99036bc6cbb03b00154b5a0bcc
Theodufthir <theo.dufthir@gmail.com>
2023-05-20 17:49:02 +0200

    Linked front to back

--- Pokedex/WebApp/Models/Pokemon.cs
+++ Pokedex/WebApp/Models/Pokemon.cs
@@ -2,7 +2,32 @@
 
 namespace WebApp.Pages
 {
-    public class Pokemon : PokedexBackend.Dbo.Pokemon
+    public class Pokemon
     {
+        public long Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public int Def { get; set; }
+
+        public int DefSpe { get; set; }
+
+        public int Attack { get; set; }
+
+        public int AttackSpe { get; set; }
+
+        public int Speed { get; set; }
+
+        public int Hp { get; set; }
+
+        public string Type1 { get; set; } = null!;
+
+        public string? Type2 { get; set; }
+
+        public string Description { get; set; } = null!;
+
+        public string? ImagePath { get; set; }
+
+        public ICollection<long> Attacks { get; set; } = new List<long>();
     }
 }
--- Pokedex/WebApp/Models/User.cs
+++ Pokedex/WebApp/Models/User.cs
@@ -2,7 +2,14 @@
 
 namespace WebApp.Pages
 {
-    public class User : PokedexBackend.Dbo.User
+    public class User
     {
+        public long Id { get; set; }
+
+        public string Username { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public ICollection<long> Pokemons { get; set; } = new List<long>();
     }
 }
