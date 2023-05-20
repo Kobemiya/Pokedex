b3d8e5bf41ce3db65f3e5fbb5baaf46b4da649b1
Yagg-Yoon <61952790+Yagg-Yoon@users.noreply.github.com>
2023-05-20 19:41:49 +0200

    removed my index

--- Pokedex/WebApp/Pages/Info.cshtml.cs
+++ Pokedex/WebApp/Pages/Info.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApp.Pages
 {
-    public class IndexModel : PageModel
+    public class InfoModel : PageModel
     {
         private readonly HttpClient _httpClient;
         public Task<IEnumerable<Pokemon>> RegisteredPokemons { get; set; }
